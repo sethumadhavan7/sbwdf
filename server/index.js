10f81031333ed9b90250a2559a6f0125d924c5e2
sethumadhavan7 <chandrabosechandrabose9@gmail.com>
2024-07-11 20:19:15 +0530

    Update index.js

--- server/index.js
+++ server/index.js
@@ -11,7 +11,7 @@ app.use(cors({
 }));
 app.use(express.json());
 
-mongoose.connect('mongodb+srv://yousaf:test123@cluster0.g4i5dey.mongodb.net/test?retryWrites=true&w=majority')
+mongoose.connect('mongodb+srv://sethumadhavan:indrajit@cluster0.dbntwx8.mongodb.net/mern-app?retryWrites=true&w=majority&appName=Cluster0')
     .then(() => console.log('MongoDB connected'))
     .catch(err => console.log(err));
 
