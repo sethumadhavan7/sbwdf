7fd90437f3eb60344b3b195f312ea8a43256c39b
sethumadhavan7 <chandrabosechandrabose9@gmail.com>
2024-07-12 14:08:48 +0530

    Update index.js

--- server/index.js
+++ server/index.js
@@ -5,7 +5,7 @@ const RegisterModel = require('./models/Register');
 
 const app = express();
 app.use(cors({
-    origin: ["https://birthdaywishdynamic-frontend.vercel.app"],
+    origin: ["https://sethuwebsite-frontend.vercel.app"],
     methods: ["POST", "GET"],
     credentials: true
 }));
